3abb00b8e49bb3a45f875ddd607f1defa87e0466
Guy Warburg <guy.warburg@gmail.com>
2017-03-05 15:08:21 +0200

    Added game-over view. Reset button still doesn’t fully work

--- day 53/js/script.js
+++ day 53/js/script.js
@@ -1,9 +1,12 @@
 const Bullets = [];
 const Invaders = [];
+var game;
 const canvas = document.getElementById("myCanvas"),
     ctx = canvas.getContext("2d");
 const MAX_SPEED = 4;
-var h,w; // hold screen height and width
+var h, w; // hold screen height and width
+var myLoop;
+
 
 /////////////////
 // Objects //////
@@ -99,6 +102,32 @@ function Bullet(x){
     };
 }
 
+function Game(){
+    var score = 0;
+    var lives = 3;
+
+    return {
+        incrementScore: function(){
+            score++;
+        },
+        // remove one life, return false if game is over
+        decrementLives: function(){
+            lives--;
+            if(lives === 0){
+                return true;
+            }
+            return false;
+        },
+        // update html with new score
+        updateScore: function(el){
+            el.innerHTML = score;
+        },
+        updateLives: function(el){
+            el.innerHTML = lives;
+        }
+    };
+}
+
 /////////////////
 // Functions ////
 /////////////////
@@ -126,6 +155,7 @@ function createInvaders(num){
 // if no invaders return true
 // if there is an invader removeVisibility of invader and return false
 function testForInvaders(bullet){
+    var score = document.getElementById('score');
     var bulletX = bullet.getLocation()[0],
         bulletY = bullet.getLocation()[1];
 
@@ -138,6 +168,8 @@ function testForInvaders(bullet){
             if(bulletX >= (invaderX - 15) && bulletX <= (invaderX + 15) &&
             bulletY >= (invaderY - 15) && bulletY <= (invaderY + 15)){
                 Invaders[i].removeVisibility();
+                game.incrementScore();
+                game.updateScore(score);
                 return false;
             }
         }
@@ -145,15 +177,44 @@ function testForInvaders(bullet){
     return true;
 }
 
+// Display Game-Over Modal
+function gameOver(){
+    // clear interval
+    while(myLoop){
+        clearInterval(myLoop);
+        myLoop--;
+    }
+    var modal = document.getElementById('modal'),
+        modalScore = document.getElementById('mScore');
+    
+    // update score and display game-over modal
+    game.updateScore(modalScore);
+    modal.style.display = 'block';
+    // add event listener to button
+    document.getElementById('try-again').addEventListener('click', function(){
+            Invaders.length = 0;
+            Bullets.length = 0;
+            init();
+    });
+}
+
 // initialize game
 function init(){
+    document.getElementById('modal').style.display = 'none';
     // Set canvas size
     h = window.innerHeight - 50; // '-50' is to make slightly smaller than view.
     w = window.innerWidth - 50;
     setAttributes(canvas, {"height": h, "width": w}); 
+    // create new game instance
+    game = new Game();
+    game.updateScore(document.getElementById('score'));
+    game.updateLives(document.getElementById('lives'));
+
     // generate player
-    var myPlayer = new Player(w, h);
-    myPlayer.draw();
+    if(!myPlayer){
+        var myPlayer = new Player(w, h);
+        myPlayer.draw();
+    }
     
     // Create initial amount of invaders
     createInvaders(30);
@@ -177,7 +238,7 @@ function init(){
             default:
                 break;
         }
-    });
+});
     // start loop
     loop(myPlayer);
 }
@@ -186,13 +247,23 @@ function init(){
 // set an interval for animation
 // move and re-draw all necessary elements after testing if visible or if 'killed'
 function loop(myPlayer){
-    window.setInterval(function(){
+    myLoop = window.setInterval(function(){
         ctx.clearRect(0, 0, w, h);
         myPlayer.draw();
         // move all invaders
         for(var i = 0; i < Invaders.length; i++){
             if(Invaders[i].isVisible()){
-                Invaders[i].move();
+                Invaders[i].move()
+                var yPos = Invaders[i].getLocation()[1];
+
+                if(yPos > h){
+                    Invaders[i].removeVisibility();
+                    var status = game.decrementLives();
+                    game.updateLives(document.getElementById('lives'));
+                    if(status){
+                        gameOver();
+                    }
+                }
             }
         }
         // move all visible bullets
@@ -220,3 +291,8 @@ function loop(myPlayer){
 
 // Start a game
 init();
+
+/////////////
+// TODO
+// - fix multiple playes on second game.
+// - test if speed increases from round to round.
