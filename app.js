4d8539b09a3c5589de3f8db7dd369f6877970842
Guy Warburg <guy.warburg@gmail.com>
2017-03-07 12:05:24 +0200

    serving static files

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/websites', express.static('websites'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
